afd7c2e60cf9e03cca7401a23f296acd5f003482
Mike Zornek <zorn@zornlabs.com>
2023-09-10 18:40:52 -0400

    Update expected Elixir language version.

--- backend-elixir/mix.exs
+++ backend-elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule StudyHall.MixProject do
     [
       app: :study_hall,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
