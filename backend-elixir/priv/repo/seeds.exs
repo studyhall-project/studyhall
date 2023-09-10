7907875ee0fbe9f32b8c3ca5fad8f9aa57570b71
Mike Zornek <zorn@zornlabs.com>
2023-09-10 19:15:25 -0400

    Update the seed file to make some sample courses.

--- backend-elixir/priv/repo/seeds.exs
+++ backend-elixir/priv/repo/seeds.exs
@@ -1,11 +1,40 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     StudyHall.Repo.insert!(%StudyHall.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias StudyHall.CourseCatalog.Course
+
+# Make some sample courses to play with.
+
+titles = [
+  "Coding and Coffee: Brewing Beautiful Websites",
+  "JavaScript Jedi Training: May the Functions Be with You",
+  "The CSS Circus: Where Styles Meet Smiles",
+  "HTML: Hilariously Tagging Markup Language",
+  "Node.js Nonsense: Making Servers Silly",
+  "React Revolution: Building Web Apps with a Twist",
+  "Sass-tastic Styles: Where Sass Meets Silliness",
+  "The Git Gurus: Committing to Comedy",
+  "Web Wonders with Wix: Creating Sites and Laughs",
+  "API Adventures: A Quest for Web Development Fun",
+  "Coding Campfire: Roasting Bugs and Marshmallows",
+  "Web Dev Wizardry: From 'Hello World' to Hilarity",
+  "The Ruby Rodeo: Riding Rails to Ridiculousness",
+  "PHP Puns and Practices: Server-Side Giggles",
+  "Python's Web Party: Flask & Django Disco",
+  "HTML Heroes: Unleash Your Markup Superpowers",
+  "Web Dev Riddles: Solving Problems with a Chuckle",
+  "Angular Antics: Fun with Front-End Frameworks",
+  "The Vue from Here: A Comedy of Errors",
+  "Joking with Java: Scripting for Fun and Funnies",
+  "Web Development Stand-Up: Laugh, Learn, Code",
+  "Docker Doodle: Container Comedy Club",
+  "Kubernetes Capers: Orchestrating Laughter",
+  "DevOps Comedy Central: From Chaos to Chuckles",
+  "Web Animation Extravaganza: CSS & Giggles",
+  "Code Bloopers and Blunders: Web Dev Edition",
+  "Web Scraping Hilarity: A Crawling Comedy Show",
+  "Web Performance Comedy Hour: Faster Sites, More Chuckles",
+  "Testing Tomfoolery: Quality Assurance Comedy",
+  "Web Dev Comedy Therapy: Healing with Humor"
+]
+
+for title <- titles do
+  %Course{} = Course.create!(%{title: title})
+end
