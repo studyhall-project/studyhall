afaff5d7a8cb5f941adb5c501eadeb0fb0e6d943
Mike Zornek <zorn@zornlabs.com>
2023-09-10 18:31:27 -0400

    Adds a Phoenix 1.7.7 application project to be the backend of the platform.

---
+++ backend-elixir/lib/study_hall/application.ex
@@ -0,0 +1,38 @@
+defmodule StudyHall.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      StudyHallWeb.Telemetry,
+      # Start the Ecto repository
+      StudyHall.Repo,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: StudyHall.PubSub},
+      # Start Finch
+      {Finch, name: StudyHall.Finch},
+      # Start the Endpoint (http/https)
+      StudyHallWeb.Endpoint
+      # Start a worker by calling: StudyHall.Worker.start_link(arg)
+      # {StudyHall.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: StudyHall.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    StudyHallWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
