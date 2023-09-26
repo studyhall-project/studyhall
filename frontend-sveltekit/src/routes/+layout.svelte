70a5bfe14640173b43710ae5e8b690671d0272f9
Mike Zornek <zorn@zornlabs.com>
2023-09-25 21:50:00 -0400

    Edit some basic page layout and add some page stubs to the navigation.

--- frontend-sveltekit/src/routes/+layout.svelte
+++ frontend-sveltekit/src/routes/+layout.svelte
@@ -14,33 +14,12 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl"><a href="/">StudyHall</a></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<a href="/support"> Support </a>
+				<a href="/sign-up"> Sign up </a>
+				<a href="/sign-in"> Sign in </a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
