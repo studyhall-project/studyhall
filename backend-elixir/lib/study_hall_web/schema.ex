5c6e321dc9f2fdb2050c4dcf8fef2e8f4255893d
Mike Zornek <zorn@zornlabs.com>
2023-10-04 17:23:33 -0400

    Create a simple graphql mutation that allows email messages to be sent for testing. Add static tests of this mutation as well (mostly for demo purposes of how to test emails).

--- backend-elixir/lib/study_hall_web/schema.ex
+++ backend-elixir/lib/study_hall_web/schema.ex
@@ -11,5 +11,14 @@ defmodule StudyHallWeb.Schema do
   end
 
   mutation do
+    @desc "Send a test email to zorn."
+    field :email_zorn, type: :email_result do
+      arg(:message, non_null(:string))
+      resolve(&StudyHallWeb.Resolvers.Email.email_zorn/3)
+    end
+  end
+
+  object :email_result do
+    field :system_message, :string
   end
 end
---
+++ backend-elixir/test/study_hall_web/api/email_zorn_test.exs
@@ -0,0 +1,27 @@
+defmodule StudyHallWeb.Api.EmailZornTest do
+  use StudyHallWeb.ConnCase
+
+  import Swoosh.TestAssertions
+
+  @email_zorn_mutation """
+  mutation emailZorn($message: String!) {
+    emailZorn(message: $message) {
+      systemMessage
+    }
+  }
+  """
+
+  test "mutation: emailZorn", %{conn: conn} do
+    conn =
+      post(conn, "/gql", %{
+        "query" => @email_zorn_mutation,
+        "variables" => %{"message" => "a simple test message"}
+      })
+
+    assert response = json_response(conn, 200)
+    assert %{"data" => %{"emailZorn" => %{"systemMessage" => "email sent"}}} = response
+
+    # Verify during resolution of this graph api call, the email was sent.
+    assert_email_sent(StudyHall.Emails.DemoMessage.new("a simple test message"))
+  end
+end
