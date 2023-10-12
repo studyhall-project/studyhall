f30e2ff13e60f8342d51fbcd4115ef93484a2947
Mike Zornek <zorn@zornlabs.com>
2023-10-12 14:20:17 -0400

    Add `ash_authentication` to the formatter dependencies.

--- backend-elixir/.formatter.exs
+++ backend-elixir/.formatter.exs
@@ -1,5 +1,14 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix, :ash, :ash_phoenix, :ash_postgres, :ash_graphql],
+  import_deps: [
+    :ash_authentication,
+    :ash_graphql,
+    :ash_phoenix,
+    :ash_postgres,
+    :ash,
+    :ecto_sql,
+    :ecto,
+    :phoenix
+  ],
   subdirectories: ["priv/*/migrations"],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
