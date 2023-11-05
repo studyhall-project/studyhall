bdba4350f804651d132813b8dbe30e586f58f70f
Mike Zornek <zorn@zornlabs.com>
2023-11-05 14:07:36 -0500

    Update temp notes with new derived actions.

--- backend-elixir/temp.ex
+++ backend-elixir/temp.ex
@@ -28,7 +28,142 @@ StudyHall.Accounts.User
 Ash.Resource.Info.actions(StudyHall.Accounts.User)
 Ash.Resource.Info.actions(StudyHall.Accounts)
 
+StudyHall.Accounts.User.request_password_reset("mike@mikezornek.com")
+
+iex(1)> Ash.Resource.Info.actions(StudyHall.Accounts.User)
 [
+  %Ash.Resource.Actions.Update{
+    name: :password_reset_with_password,
+    primary?: false,
+    description: nil,
+    error_handler: nil,
+    accept: [],
+    manual: nil,
+    manual?: false,
+    atomics: [],
+    require_attributes: [],
+    delay_global_validations?: false,
+    skip_global_validations?: false,
+    arguments: [
+      %Ash.Resource.Actions.Argument{
+        allow_nil?: true,
+        type: Ash.Type.String,
+        name: :reset_token,
+        default: nil,
+        private?: false,
+        sensitive?: true,
+        description: nil,
+        constraints: [allow_empty?: false, trim?: true]
+      },
+      %Ash.Resource.Actions.Argument{
+        allow_nil?: false,
+        type: Ash.Type.String,
+        name: :password,
+        default: nil,
+        private?: false,
+        sensitive?: true,
+        description: nil,
+        constraints: [allow_empty?: false, trim?: true, min_length: 8]
+      },
+      %Ash.Resource.Actions.Argument{
+        allow_nil?: false,
+        type: Ash.Type.String,
+        name: :password_confirmation,
+        default: nil,
+        private?: false,
+        sensitive?: true,
+        description: nil,
+        constraints: [allow_empty?: false, trim?: true, min_length: 8]
+      }
+    ],
+    changes: [
+      %Ash.Resource.Validation{
+        validation: {AshAuthentication.Strategy.Password.ResetTokenValidation,
+         []},
+        module: AshAuthentication.Strategy.Password.ResetTokenValidation,
+        opts: [],
+        only_when_valid?: false,
+        description: nil,
+        message: nil,
+        before_action?: false,
+        where: [],
+        on: []
+      },
+      %Ash.Resource.Validation{
+        validation: {AshAuthentication.Strategy.Password.PasswordConfirmationValidation,
+         []},
+        module: AshAuthentication.Strategy.Password.PasswordConfirmationValidation,
+        opts: [],
+        only_when_valid?: false,
+        description: nil,
+        message: nil,
+        before_action?: false,
+        where: [],
+        on: []
+      },
+      %Ash.Resource.Change{
+        change: {AshAuthentication.Strategy.Password.HashPasswordChange, []},
+        on: nil,
+        only_when_valid?: false,
+        description: nil,
+        where: []
+      },
+      %Ash.Resource.Change{
+        change: {AshAuthentication.GenerateTokenChange, []},
+        on: nil,
+        only_when_valid?: false,
+        description: nil,
+        where: []
+      }
+    ],
+    reject: [],
+    metadata: [
+      %Ash.Resource.Actions.Metadata{
+        allow_nil?: false,
+        type: Ash.Type.String,
+        name: :token,
+        default: nil,
+        description: nil,
+        constraints: [allow_empty?: false, trim?: true]
+      }
+    ],
+    transaction?: true,
+    touches_resources: [],
+    type: :update
+  },
+  %Ash.Resource.Actions.Read{
+    arguments: [
+      %Ash.Resource.Actions.Argument{
+        allow_nil?: false,
+        type: Ash.Type.CiString,
+        name: :email,
+        default: nil,
+        private?: false,
+        sensitive?: false,
+        description: "The proposed identity to send reset instructions to.",
+        constraints: [casing: nil, allow_empty?: false, trim?: true]
+      }
+    ],
+    description: "Send password reset instructions to a user if they exist.",
+    filter: nil,
+    get_by: [],
+    get?: false,
+    manual: nil,
+    metadata: [],
+    modify_query: nil,
+    name: :request_password_reset,
+    pagination: false,
+    preparations: [
+      %Ash.Resource.Preparation{
+        preparation: {AshAuthentication.Strategy.Password.RequestPasswordResetPreparation,
+         []}
+      }
+    ],
+    primary?: false,
+    touches_resources: [],
+    transaction?: false,
+    type: :read
+  },
   %Ash.Resource.Actions.Read{
     arguments: [
       %Ash.Resource.Actions.Argument{
@@ -118,12 +253,12 @@ Ash.Resource.Info.actions(StudyHall.Accounts)
     ],
     changes: [
       %Ash.Resource.Validation{
-        validation: {AshAuthentication.Strategy.Password.PasswordConfirmationValidation, []},
+        validation: {AshAuthentication.Strategy.Password.PasswordConfirmationValidation,
+         []},
         module: AshAuthentication.Strategy.Password.PasswordConfirmationValidation,
         opts: [],
         only_when_valid?: false,
-        description:
-          "Confirm that the values of `:password` and `:password_confirmation` are the same if confirmation is enabled.",
+        description: "Confirm that the values of `:password` and `:password_confirmation` are the same if confirmation is enabled.",
         message: nil,
         before_action?: false,
         where: [],
@@ -133,16 +268,14 @@ Ash.Resource.Info.actions(StudyHall.Accounts)
         change: {AshAuthentication.Strategy.Password.HashPasswordChange, []},
         on: nil,
         only_when_valid?: false,
-        description:
-          "Generate a cryptographic hash of the user's plain text password and store it in the `:hashed_password` attribute.",
+        description: "Generate a cryptographic hash of the user's plain text password and store it in the `:hashed_password` attribute.",
         where: []
       },
       %Ash.Resource.Change{
         change: {AshAuthentication.GenerateTokenChange, []},
         on: nil,
         only_when_valid?: false,
-        description:
-          "If token generation is enabled, generate a token and store it in the user's metadata.",
+        description: "If token generation is enabled, generate a token and store it in the user's metadata.",
         where: []
       }
     ],
@@ -194,5 +327,36 @@ Ash.Resource.Info.actions(StudyHall.Accounts)
     touches_resources: [],
     transaction?: false,
     type: :read
+  },
+  %Ash.Resource.Actions.Read{
+    arguments: [
+      %Ash.Resource.Actions.Argument{
+        allow_nil?: false,
+        type: Ash.Type.UUID,
+        name: :id,
+        default: nil,
+        private?: false,
+        sensitive?: false,
+        description: nil,
+        constraints: []
+      }
+    ],
+    description: nil,
+    filter: id == {:_arg, :id},
+    get_by: [],
+    get?: true,
+    manual: nil,
+    metadata: [],
+    modify_query: nil,
+    name: :by_id,
+    pagination: false,
+    preparations: [],
+    primary?: false,
+    touches_resources: [],
+    transaction?: false,
+    type: :read
   }
 ]
+iex(2)>
+nil
+iex(3)>
